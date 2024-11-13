de6cb0258e102618b49d5108d89d8c72ed78bc8b
Farzine <farzine07@student.sust.edu>
2024-11-13 23:57:22 +0600

    initial commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Gaibandha Student Association, SUST",
+  description: "Gaibandha Student Association, SUST is a non-profit organization of the students of Shahjalal University of Science and Technology (SUST) who are from Gaibandha district. The organization was established in 2012 with the aim of bringing together the students of Gaibandha district studying at SUST and creating a platform for them to work together for the welfare of the people of Gaibandha district.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ server/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const cors = require('cors'); 
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const frontUrl = process.env.NEXT_PUBLIC_APP_FRONTEND_URL;
+
+const { MONGO_URI } = require('./config/config');
+
+
+const corsOptions ={
+  origin:`${frontUrl}`, 
+  methods:['GET','POST','PUT','DELETE'],
+  credentials:true,            
+  optionSuccessStatus:200,
+}
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors(corsOptions));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to ARCES_2024');
+});
+
+mongoose.connect(MONGO_URI)
+  .then(async () => {
+    console.log('Connected to MongoDB');
+    await initializeAdmin(); 
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
